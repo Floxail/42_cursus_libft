2bd528514a4d17b6133f38bfe4497db3070b7ef5
Floxail <floxail@MacBook-Pro-de-Floxail.local>
2025-10-13 09:11:31 +0200

    make error correction

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flox <flox@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: floxail <floxail@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 17:33:32 by flox              #+#    #+#             */
-/*   Updated: 2025/10/11 13:15:52 by flox             ###   ########.fr       */
+/*   Updated: 2025/10/13 09:09:04 by floxail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstclear(t_list *lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp;
 
