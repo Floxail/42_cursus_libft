0c6ff29f81b7ead35a81225d6a23b8a7714db562
Florent Vejux <flvejux@c1r9s7.42lausanne.ch>
2025-11-19 13:02:23 +0100

    add ATOL

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: flvejux <flvejux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 14:59:23 by flvejux           #+#    #+#             */
-/*   Updated: 2025/10/09 11:16:46 by flvejux          ###   ########.fr       */
+/*   Updated: 2025/11/19 10:34:30 by flvejux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,32 @@ int	ft_atoi(const char *nptr)
 		nb = (nb * 10) + (nptr[i] - '0');
 		i++;
 	}
+	return (sign * nb);
+}
+
+long	ft_atol(const char *nptr)
+{
+	int		sign;
+	long	nb;
+	int		i;
+
+	sign = 1;
+	nb = 0;
+	i = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (ft_isdigit(nptr[i]))
+	{
+		nb = (nb * 10) + (nptr[i] - '0');
+		i++;
+	}
+	if ((nb * sign) > INT_MAX || (nb * sign) < INT_MIN)
+		return (FALSE);
 	return (sign * nb);
 }
